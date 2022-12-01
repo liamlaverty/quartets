5c862c44ac1bb3dbb5bf3a4ac3ead344e60a1530
Liam Laverty <liamlaverty@gmail.com>
2022-12-01 13:47:34 +0000

    renders a canvas with cubes on it

--- src/application/game.ts
+++ src/application/game.ts
@@ -5,11 +5,17 @@ import { IHtmlService } from "../core/graphics/html/IHtmlService";
 export class Game {
 
     private running = false;
-    // private htmlService: IHtmlService;
+    private golArray: boolean[][];
+    private mainCanvas: HTMLCanvasElement;
+    private mainCanvasCtx: CanvasRenderingContext2D;
 
-    constructor(
-        @inject("IHtmlService") private htmlService: IHtmlService
-        ){
+    private __CANVAS_WIDTH__ = 750;
+    private __CANVAS_HEIGHT__ = 750;
+
+    private __ARRAY_CELLS_WIDE__ = 35;
+    private __ARRAY_CELLS_HIGH__ = 35;
+
+    constructor(@inject("IHtmlService") private htmlService: IHtmlService) {
     }
 
     Run(): void {
@@ -23,11 +29,63 @@ export class Game {
 
     private Init(): void {
         console.log('Init has been called');
+        
+        this.mainCanvas = this.htmlService.createCanvas('game-of-life', this.htmlService.getMainDiv().id, this.__CANVAS_WIDTH__, this.__CANVAS_HEIGHT__, ['game-of-life-canvas']);
+        this.mainCanvasCtx = this.mainCanvas.getContext('2d');
+
+        this.golArray = [];
+        const cellHeight = this.__CANVAS_HEIGHT__ / this.__ARRAY_CELLS_HIGH__;
+        const cellWidth = this.__CANVAS_WIDTH__ / this.__ARRAY_CELLS_WIDE__;
+
+        this.mainCanvasCtx.fillStyle = '#FFFFFF';
+        this.mainCanvasCtx.fillRect(0, 0, this.__CANVAS_HEIGHT__, this.__CANVAS_HEIGHT__);
+
+        // this.mainCanvasCtx.strokeStyle = '#ff0000';
+
+        for (let x = 0; x < this.__ARRAY_CELLS_HIGH__; x++) {
+            this.golArray[x] = [];
+            for (let y = 0; y < this.__ARRAY_CELLS_WIDE__; y++) {
+                this.golArray[x][y] = false;
+                const drawLocationY = x * cellWidth;
+                const drawLocationX = y * cellHeight;
+                
+                this.mainCanvasCtx.strokeRect(drawLocationX, drawLocationY, cellWidth, cellHeight);
+                console.log('drawing at ' + ' ' + drawLocationX + ' ' + drawLocationY + ' ' + cellWidth + ' ' + cellHeight);
+            }
+        }
+        
+        this.running = true;
     }
 
 
     private Loop() {
         console.log('Loop has been called');
+
+        requestAnimationFrame(() => {
+
+            // this.mainCanvasCtx.fillStyle = '#000000';
+            // this.mainCanvasCtx.fillRect(0, 0, this.__CANVAS_HEIGHT__, this.__CANVAS_HEIGHT__);
+
+
+
+
+            for (let x = 0; x < this.__ARRAY_CELLS_HIGH__; x++) {
+                for (let y = 0; y < this.__ARRAY_CELLS_WIDE__; y++) {
+                    // if (this.golArray[x][y] === true){
+                    //   a  
+                    // } 
+
+
+                    //this.mainCanvasCtx.fillRect(0, 0, this.__CANVAS_HEIGHT__, this.__CANVAS_HEIGHT__);
+                }
+            }
+
+            // if (this.running){
+
+            // }
+
+            // this.Loop();
+        });
     }
 
 }
